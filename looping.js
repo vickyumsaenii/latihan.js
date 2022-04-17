1fae81bf31b6bbab11de69883d51daa2cf575594
vickyumsaenii <vicky.umsaenii@gmail.com>
2022-04-17 14:35:03 +0700

    looping

--- looping.js
+++ looping.js
@@ -1,5 +1,54 @@
-// loping
+// // loping
 
-for (var 1 =i < 5; i++) {
-  console.log(i)
+// for (var i = 1; < 10; i += 1) {
+//   console.log(i)
+// }
+
+/**
+ * STUDY CASE 1
+ *
+ * VAR STR = 'BUKBER
+ *
+ *
+ * Result:
+ * BuKbEr
+ */
+// var str = "bukber";
+// var temp = "';
+
+// for (var i = 0; i < str.length; i++) {
+//   if (1 % 2 === 0) {
+//     //console.log(str[i].toUpperCase());
+//     temp += str[i].toUpperCase();
+//   } else {
+//     temp += str[i];
+//     //console.log(str[i]);
+//   }
+// }
+// console.log(temp);
+
+// var kode = 'abaabbabab';
+// var countA = 0;
+// var countB = 0;
+
+// for (var i = 0; i < kode.length; i++) {
+//   if (kode[i] === 'a') {
+//     countA++;
+//   } else if (kode[i] === 'b') {
+//     countB++;
+//   }
+// }
+// console.log('jumlah a= ' + countA);
+// console.log('jumlah b= ' + countB);
+
+var kalimat = 'saya mau buka bersama teman';
+
+for (var i = 0; i < kalimat.length; i++) {
+  if (kalimat[1] !== '') {
+    temp += kalimat[1];
+    // console.log(kalimat[i])
+  } else {
+    console.log(temp);
+    temp = '';
+  }
 }
